2c83b65778e0f0b0aa3f8a5650da053b9998a836
PragmaFlow <steven.mckeown@pragmaflow.com>
2025-02-11 15:13:58 -0500

    Update project.cpp

--- src/license_generator/project.cpp
+++ src/license_generator/project.cpp
@@ -52,7 +52,7 @@ static const string guess_templates_folder(const string &source_folder) {
 									 "] does not exist. tried also [" + template_fname.string() + "]");
 		}
 	}
-	fs::path normalized = templates_path.normalize();
+	fs::path normalized = fs::weakly_canonical(templates_path);
 	return normalized.string();
 }
 static const fs::path publicKeyFolder(const fs::path &product_folder, const string &product_name) {
